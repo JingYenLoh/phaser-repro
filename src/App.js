c2483a961275972881032c2d3909e453110369e8
Jing Yen Loh <lohjingyen@gmail.com>
2020-06-07 04:30:52 +0800

    Create example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Phaser from 'phaser';
+import { IonPhaser } from '@ion-phaser/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonPhaser game={{ width: "100%", height: "100%", type: Phaser.AUTO, scene: {} }} initialize={false} />
   );
 }
 
